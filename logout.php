16ecd0f79480532a04a3ce367c9afe4dbe73dd39
sumad200 <92201689+sumad200@users.noreply.github.com>
2021-10-20 14:57:14 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,12 @@
 <?php
 // Initialize the session.
-// If you are using session_name("something"), don't forget it now!
 session_start();
 
-// Unset all of the session variables.
+
 $_SESSION = array();
 
-// If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
+
+// This will destroy the session, and not just the session data!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -16,7 +15,7 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Finally, destroy the session.
+// destroy the session.
 session_destroy();
 echo "<script>
 alert('You have logged out successfully');
